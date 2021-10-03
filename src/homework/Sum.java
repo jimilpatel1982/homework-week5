bc370cbc63f007a88f4351534cba95aafb23084d
jimilpatel1982 <91703283+jimilpatel1982@users.noreply.github.com>
2021-10-03 22:38:49 +0100

    Add files via upload

---
+++ src/homework/Sum.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class Sum {
+
+    public static void main(String[] args) {
+
+        int a = 74;
+        int b = 36;
+
+        System.out.println("Test Data: "+a+" + "+b);
+        System.out.println("\033[3mExpected Output: \033[0m"+(a+b));
+
+    }
+
+}
